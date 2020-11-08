a2fcdb92ae2ace86f1ed63f5386062fd73975c19
Baiqu-an <qh19445@bristol.ac.uk>
2020-11-08 16:31:39 +0000

    maven good?

---
+++ demo/src/test/java/com/baiPrac/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baiPrac.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
